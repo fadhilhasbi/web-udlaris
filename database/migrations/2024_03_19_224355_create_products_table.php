e7419022a6677bc599a51d0733c4966a444ece00
fadhilhasbi <muhammad1900016050@webmail.uad.ac.id>
2024-03-20 06:00:16 +0700

    feat(cms): add CRUD product data

---
+++ database/migrations/2024_03_19_224355_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('sku')->unique();
+            $table->string('image')->nullable();
+            $table->longText('description')->nullable();
+            $table->unsignedBigInteger('quantity');
+            $table->decimal('price', 15, 2);
+            $table->decimal('weight', 10, 2);
+            $table->decimal('length', 10, 2);
+            $table->decimal('width', 10, 2);
+            $table->decimal('height', 10, 2);
+            $table->boolean('is_active')->default(false);
+            $table->boolean('is_featured')->default(false);
+            $table->date('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
