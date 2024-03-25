5ce4d99106b8c857e6902cab33f70185c3e058df
fadhilhasbi <muhammad1900016050@webmail.uad.ac.id>
2024-03-25 11:31:41 +0700

    feat(homepage): change hero background

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -2,7 +2,7 @@
     <section class="mb-40 overflow-hidden">
         <!-- Background image -->
         <div
-            class="relative overflow-hidden bg-cover bg-no-repeat bg-[50%] h-[500px] bg-[url('https://tecdn.b-cdn.net/img/new/standard/city/078.jpg')]">
+            class="relative overflow-hidden bg-cover bg-no-repeat bg-[50%] h-[500px] bg-[url('https://img.archiexpo.com/images_ae/photo-g/11279-18521817.webp')]">
             <div
                 class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed bg-[hsla(0,0%,0%,0.75)]">
                 <div class="flex h-full items-center justify-center">
@@ -11,7 +11,7 @@
                             Cocokan kebutuhanmu dengan <br /><span>kustomisasi</span>
                         </h1>
                         <a class="mb-2 inline-block rounded-full border-2 border-neutral-50 px-[46px] pt-[14px] pb-[12px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 md:mr-2 md:mb-0"
-                            data-te-ripple-init data-te-ripple-color="light" href="#" role="button">Mulai
+                            data-te-ripple-init data-te-ripple-color="light" href="/product-custom" wire:navigate role="button">Mulai
                             kustomisasi</a>
                     </div>
                 </div>
