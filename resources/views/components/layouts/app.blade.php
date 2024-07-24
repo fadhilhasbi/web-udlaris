c28fe38bb2be785059e24ceca8824a9d14b7754f
fadhilhasbi <muhammad1900016050@webmail.uad.ac.id>
2024-07-24 15:26:25 +0700

    fix(custom): change cdn of x3d

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,11 +8,11 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        <script type='text/javascript' src='http://www.x3dom.org/download/x3dom.js'> </script>
+        <script type='text/javascript' src='https://www.x3dom.org/download/x3dom.js'> </script>
 
         <!-- Styles -->
         @livewireStyles
-        <link rel='stylesheet' type='text/css' href='http://www.x3dom.org/download/x3dom.css'></link>
+        <link rel='stylesheet' type='text/css' href='https://www.x3dom.org/download/x3dom.css'></link>
     </head>
     <body x-cloak x-data="{darkMode: $persist(false)}" :class="{'dark': darkMode === true }" class="antialiased">
         @livewire('partials.navbar')
