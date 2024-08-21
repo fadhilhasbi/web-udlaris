cb9c0b830bd52daef244d9e87e9173d5b1890064
fadhilhasbi <muhammad1900016050@webmail.uad.ac.id>
2024-08-21 09:44:50 +0700

    feat(transaction): draft feature after payment 1.1

--- app/Livewire/CancelPage.php
+++ app/Livewire/CancelPage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Order;
 use Livewire\Component;
 
 class CancelPage extends Component
 {
+    public $order;
+
+    public function mount($order_id)
+    {
+        $this->order = Order::findOrFail($order_id);
+    }
+
     public function render()
     {
-        return view('livewire.cancel-page');
+        return view('livewire.cancel-page', [
+            'paymentStatus' => $this->order->payment_status,
+        ]);
     }
 }
--- app/Livewire/SuccessPage.php
+++ app/Livewire/SuccessPage.php
@@ -11,13 +11,13 @@ class SuccessPage extends Component
 
     public function mount($order_id)
     {
-        $this->order = Order::with('address')->findOrFail($order_id);
+        $this->order = Order::findOrFail($order_id);
     }
 
     public function render()
     {
         return view('livewire.success-page', [
-            'order' => $this->order,
+            'paymentStatus' => $this->order->payment_status,
         ]);
     }
 }
