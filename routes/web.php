c5ab4e7f0576f02f34140d00b2985feba994b81f
hamim <hamimaz326@gmail.com>
2024-08-23 21:03:17 +0700

    feat(transaction): add customer order page

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,22 @@
 <?php
 
-use App\Livewire\CartPage;
-use App\Livewire\HomePage;
 use App\Livewire\CancelPage;
-use App\Livewire\SuccessPage;
 use App\Livewire\CheckoutPage;
-use App\Livewire\ProductsPage;
-use App\Livewire\CustomRakPage;
-use App\Livewire\CategoriesPage;
-use App\Livewire\CustomChairPage;
+use App\Livewire\CustomTableMejaPanjang;
 use App\Livewire\CustomTablePage;
-use App\Livewire\CustomCabinetPage;
+use App\Livewire\HomePage;
+use App\Livewire\MyOrdersPage;
 use App\Livewire\ProductCustomPage;
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
 use App\Livewire\ProductDetailPage;
-use App\Livewire\CustomCreateRakPage;
+use App\Livewire\ProductsPage;
+
+use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
-use App\Livewire\CustomCreateChairPage;
-use App\Livewire\CustomCreateTablePage;
-use App\Livewire\CustomCreateCabinetPage;
-use App\Http\Controllers\MidtransController;
 
 /*
-|-------------------------------------------------------->>>>>>> develop------------------
+|--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
@@ -36,22 +31,14 @@ Route::get('/categories', CategoriesPage::class);
 Route::get('/products', ProductsPage::class);
 Route::get('/products/{slug}', ProductDetailPage::class);
 Route::get('/cart', CartPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/success', SuccessPage::class)->name('success');
+Route::get('/cancel', [CancelPage::class, 'cancel'])->name('cancel');
+Route::get('/my-order', MyOrdersPage::class)->name('orders');
 
 Route::get('/product-custom', ProductCustomPage::class);
 Route::get('/product-custom/meja', CustomTablePage::class);
-Route::get('/product-custom/rak', CustomRakPage::class);
-Route::get('/product-custom/kursi', CustomChairPage::class);
-Route::get('/product-custom/lemari', CustomCabinetPage::class);
-Route::get('/product-custom/meja/{slug}', CustomCreateTablePage::class);
-Route::get('/product-custom/rak/{slug}', CustomCreateRakPage::class);
-Route::get('/product-custom/kursi/{slug}', CustomCreateChairPage::class);
-Route::get('/product-custom/lemari/{slug}', CustomCreateCabinetPage::class);
-
-
-Route::get('/categories', CategoriesPage::class);
-Route::get('/products', ProductsPage::class);
-Route::get('/products/{product}', ProductDetailPage::class);
-Route::get('/cart', CartPage::class);
+// Route::get('/product-custom/meja/meja-panjang', CustomTableMejaPanjang::class);
 
 Route::middleware([
     'auth:sanctum',
@@ -61,8 +48,4 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
-    Route::get('/checkout', CheckoutPage::class);
-    Route::get('/checkout/success/{order_id}', SuccessPage::class)->name('success');
-    Route::get('/checkout/cancel/{order_id}', CancelPage::class)->name('cancel');
-    Route::post('/midtrans/notification', [MidtransController::class, 'handleNotification'])->name('midtrans.notification');
 });
